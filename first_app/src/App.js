22aec5f81dde9168962486536a98be28131aec4c
RifqiHanif <rifqihanif24@gmail.com>
2023-03-11 10:03:14 +0700

    uploading training for searching

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+import { Grid, TextField } from "@mui/material";
+import React, { useState, useEffect } from "react";
 import './App.css';
 
+
 function App() {
+  const [state, setState]= useState("");
+  const data = [
+    {
+      id:1,
+      name: "krisna",
+      major: "informatic"
+    },
+    {
+      id:2,
+      name: "andi",
+      major: "ipa"
+    },
+    {
+      id:3,
+      name: "kusen",
+      major: "matematika"
+    },
+  ];
+
+  const result = data.filter((x)=> x.name.toLowerCase().includes(state.toLowerCase()));
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <box>
+      <Grid container>
+        {state}
+        <Grid xs={12} sx={{margin:10}}>
+          <TextField 
+          label ="Search"
+          onChange={(e)=>{
+            setState(e.target.value)
+          }}/>
+        </Grid>
+        <Grid xs={12} sx={{margin:10}}>
+          <table>
+            <tr>
+              <th>Name</th>
+              <th>Major</th>
+            </tr>
+            {state 
+              ? result.map((x)=>{
+                return(
+                  <tr>
+                    <td>{x.name}</td>
+                    <td>{x.major}</td>
+                  </tr>
+                );
+            }): data.map((x)=>{
+              return(
+              <tr>
+                  <td>{x.name}</td>
+                  <td>{x.major}</td>
+              </tr>)
+            })}
+          </table>
+        </Grid>
+      </Grid>
+    </box>
   );
 }
 
